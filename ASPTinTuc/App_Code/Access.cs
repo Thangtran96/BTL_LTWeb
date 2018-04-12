3d4c13d0a0f0b5c3e76f13aaf4fff9c721274d18
DESKTOP-MT\ThangTran <minhthang.tran.398.15@gmail.com>
2018-04-13 01:12:29 +0700

    cuoi ngay 23/04/2018 làm access database

---
+++ ASPTinTuc/App_Code/Access.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ASPTinTuc.App_Code
+{
+    public class Access
+    {
+        private string ConnectString = ConfigurationManager.ConnectionStrings["ConnectString"].ConnectionString;
+        //ham tra ve string ket noi
+        public SqlConnection GetConnect()
+        {
+            return new SqlConnection(ConnectString);
+        }
+        //ham tra ve 1 datatable
+        public DataTable GetTable(string sql)
+        {
+            SqlConnection conn = GetConnect();
+            SqlDataAdapter da = new SqlDataAdapter(conn, sql);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+    }
+}
